e3985dfccb6b63077e49e343e862826ec3832189
Lakindu Hesara Perera <159991932+lak-git@users.noreply.github.com>
2025-02-03 16:57:16 +0530

    Fork Modification

--- SDAM2_LMS/Properties/Resources.Designer.cs
+++ SDAM2_LMS/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SDAM2_LMS.Properties {
+namespace LibraryManagementSystem.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SDAM2_LMS.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SDAM2_LMS.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LibraryManagementSystem.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
